99d8fcf0bdce55aae8e10d7dbe54ba1ae02d8e0d
troyraen <troy.raen@pitt.edu>
2021-10-04 19:16:24 -0400

    update stream rest. this works, locally.

--- tom_pittgoogle/broker_stream_rest.py
+++ tom_pittgoogle/broker_stream_rest.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-"""Pitt-Google broker module for TOM Toolkit."""
+"""Pitt-Google broker module for TOM Toolkit.
+
+Connects to the user's Pub/Sub subscription via the REST API.
+
+API Docs: https://cloud.google.com/pubsub/docs/reference/rest
+"""
 
 from django import forms
 from tom_alerts.alerts import GenericQueryForm, GenericAlert, GenericBroker
 
-from consumer_stream_rest import PittGoogleConsumer
+from .consumer_stream_rest import PittGoogleConsumer
+from .utils.templatetags.utility_tags import jd_to_readable_date
 
 
-CONSUMER = PittGoogleConsumer()
+SUBSCRIPTION_NAME = "ztf-loop"
+# Create the subscription if needed, and make sure we can connect to it.
+CONSUMER = PittGoogleConsumer(SUBSCRIPTION_NAME)
 
 
 class FilterAlertsForm(GenericQueryForm):
@@ -16,35 +24,62 @@ class FilterAlertsForm(GenericQueryForm):
 
     Fields:
         max_results (``IntegerField``)
+        classtar_threshold (``FloatField``)
+        classtar_gt_lt (``ChoiceField``)
     """
 
     max_results = forms.IntegerField(
         required=True, initial=10, min_value=1, max_value=1000
     )
+    classtar_threshold = forms.FloatField(
+        required=False,
+        initial=0.5,
+        min_value=0,
+        max_value=1,
+        help_text="Star/Galaxy score threshold",
+    )
+    classtar_gt_lt_choices = [("lt", "less than"), ("gt", "greater than or equal")]
+    classtar_gt_lt = forms.ChoiceField(
+        required=True,
+        choices=classtar_gt_lt_choices,
+        initial="lt",
+        widget=forms.RadioSelect,
+        label="",
+    )
 
 
 class PittGoogleBroker(GenericBroker):
-    """Pitt-Google broker interface to fetch alerts."""
+    """Pitt-Google broker interface to pull alerts from Pub/Sub via the REST API."""
 
     name = "Pitt-Google"
     form = FilterAlertsForm
 
-    @classmethod
     def fetch_alerts(self, parameters):
         """Pull or query alerts, unpack, apply the user filter, return an iterator."""
         clean_params = self._clean_parameters(parameters)
 
-        alerts = []
-        while len(alerts) < parameters['max_results']:
+        alerts, i, max_tries = [], 0, 5  # avoid trying forever
+        while (len(alerts) < parameters['max_results']) & (i < max_tries):
+            i += 1
+            print(i)
+            print(len(alerts))
+            clean_params['max_results'] = parameters['max_results'] - len(alerts)
             alerts += self._request_alerts(clean_params)  # List[dict]
 
         return iter(alerts)
 
     def _request_alerts(self, parameters):
-        """Pull or query alerts, unpack, apply the user filter."""
-        response = CONSUMER.oauth.post()
+        """Pull alerts using a POST request with OAuth, unpack, apply user filter.
+
+        All messages (alerts) are automatically acknowledged, and therefore leave the
+        subscription, unless there is an error.
+        """
+        response = CONSUMER.oauth.post(
+            f"{CONSUMER.subscription_url}:pull",
+            data={"maxMessages": parameters["max_results"]},
+        )
         response.raise_for_status()
-        alerts = CONSUMER.unpack_messages(
+        alerts = CONSUMER.unpack_and_ack_messages(
             response,
             lighten_alerts=True,
             callback=self._user_filter,
@@ -53,29 +88,47 @@ class PittGoogleBroker(GenericBroker):
         return alerts
 
     @staticmethod
-    def _user_filter(alert, parameters):
+    def _user_filter(alert_dict, parameters):
         """Apply the filter indicated by the form's parameters.
+
+        Used as the `callback` to `CONSUMER.unpack_and_ack_messages`.
 
         Args:
             `alert_dict`: Single alert, ZTF packet data.
+                          The schema depends on the value of `lighten_alerts` passed to
+                          `CONSUMER.unpack_and_ack_messages`.
+                          If `lighten_alerts=False` it is the original ZTF alert schema
+                          (https://zwickytransientfacility.github.io/ztf-avro-alert/schema.html)
+                          If `lighten_alerts=True` the dict is flattened and only
+                          includes keys saved by `CONSUMER._lighten_alert`.
+
             `parameters`: parameters submitted by the user through the form.
+
+        Returns:
+            `alert_dict` if it passes the filter, else `None`
         """
-        alert_passes_filter = True
-        if alert_passes_filter:
-            return alert
+        if parameters["classtar_threshold"] is None:
+            # no filter requested. all alerts pass
+            return alert_dict
+
+        # run the filter
+        classtar_lt_thresh = alert_dict["classtar"] < parameters["classtar_threshold"]
+        if ((parameters["classtar_gt_lt"] == "lt") & classtar_lt_thresh) or (
+            (parameters["classtar_gt_lt"] == "gt") & ~classtar_lt_thresh
+        ):
+            return alert_dict
         else:
             return None
 
-    def _clean_parameters(parameters):
+    def _clean_parameters(self, parameters):
         clean_params = dict(parameters)
         return clean_params
 
-    @classmethod
     def to_generic_alert(self, alert):
         """Map the Pitt-Google alert to a TOM `GenericAlert`."""
         return GenericAlert(
-            timestamp=alert["jd"],
-            url="https://{service}.googleapis.com/{resource_path}",
+            timestamp=jd_to_readable_date(alert["jd"]),
+            url=CONSUMER.subscription_url,
             id=alert["candid"],
             name=alert["objectId"],
             ra=alert["ra"],
--- tom_pittgoogle/consumer_stream_rest.py
+++ tom_pittgoogle/consumer_stream_rest.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-"""Consumer class to pull or query alerts from Pitt-Google."""
+"""Consumer class to pull alerts from a Pitt-Google message stream.
+
+Connects to the user's Pub/Sub subscription via the REST API.
+
+API Docs: https://cloud.google.com/pubsub/docs/reference/rest
+"""
 
 from django.conf import settings
+import json
 from requests_oauthlib import OAuth2Session
 
 from .utils.templatetags.utility_tags import b64avro_to_dict
@@ -12,7 +18,7 @@ PITTGOOGLE_PROJECT_ID = "ardent-cycling-243415"
 
 
 class PittGoogleConsumer:
-    """Consumer class to pull or query alerts from Pitt-Google, and manipulate them."""
+    """Manage a subscription that is linked to a topic published by Pitt-Google."""
 
     def __init__(self, subscription_name):
         """Open a subscriber client. If the subscription doesn't exist, create it.
@@ -27,11 +33,22 @@ class PittGoogleConsumer:
         self._authenticate()
 
         # logger
-        # TODO: add needed params/logic
+        # TODO: debug the logger. it currently doesn't not work
+        self.logging_url = "https://logging.googleapis.com/v2/entries:write"
+        self.log_name = (
+            f"projects/{settings.GOOGLE_CLOUD_PROJECT}/logs/{subscription_name}"
+        )
 
-        # subscription or table resource
-        # TODO: add needed params/logic
-        self._get_resource()
+        # subscription
+        self.subscription_name = subscription_name
+        self.subscription_path = (
+            f"projects/{settings.GOOGLE_CLOUD_PROJECT}/subscriptions/{subscription_name}"
+        )
+        self.subscription_url = (
+            f"https://pubsub.googleapis.com/v1/{self.subscription_path}"
+        )
+        self.topic_path = ""  # for user info only. set in _get_create_subscription()
+        self._get_create_subscription()
 
     def _authenticate(self):
         """Authenticate the user via OAuth 2.0.
@@ -45,6 +62,7 @@ class PittGoogleConsumer:
         redirect_uri = "https://ardent-cycling-243415.appspot.com/"  # TODO: better page
         scopes = [
             "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/pubsub",
         ]
         oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scopes)
 
@@ -55,7 +73,7 @@ class PittGoogleConsumer:
             prompt="select_account",
         )
         print(
-            f"Please visit this URL to authorize PittGoogleConsumer:\n{authorization_url}"
+            f"Please visit this URL to authorize PittGoogleConsumer:\n\n{authorization_url}\n"
         )
         authorization_response = input(
             "Enter the full URL of the page you are redirected to after authorization:\n"
@@ -69,17 +87,70 @@ class PittGoogleConsumer:
         )
         self.oauth = oauth
 
-    def _get_resource(self):
-        """Make sure the resource exists, and we can connect to it."""
-        # TODO: add needed logic
-        return
+    def _get_create_subscription(self):
+        """Create the subscription if needed.
 
-    def unpack_messages(
+        1.  Check whether a subscription exists in the user's GCP project.
+
+        2.  If it doesn't, try to create a subscription in the user's project that is
+            attached to a topic of the same name in the Pitt-Google project.
+        """
+        # check if subscription exists
+        get_response = self.oauth.get(self.subscription_url)
+
+        if get_response.status_code == 200:
+            # subscription exists. tell the user which topic it's connected to.
+            self.topic_path = json.loads(get_response.content)["topic"]
+            print(f"Subscription exists: {self.subscription_path}")
+            print(f"Connected to topic: {self.topic_path}")
+
+        elif get_response.status_code == 404:
+            # subscription doesn't exist. try to create it.
+            self._create_subscription()
+
+        else:
+            print(get_response.content)
+            get_response.raise_for_status()
+
+    def _create_subscription(self):
+        """Try to create the subscription."""
+        topic_path = f"projects/{PITTGOOGLE_PROJECT_ID}/topics/{self.subscription_name}"
+        request = {"topic": topic_path}
+        put_response = self.oauth.put(f"{self.subscription_url}", data=request)
+
+        if put_response.status_code == 200:
+            # subscription created successfully
+            self.topic_path = topic_path
+            self._log_and_print(
+                (
+                    f"Created subscription: {self.subscription_path}\n"
+                    f"Connected to topic: {self.topic_path}"
+                )
+            )
+
+        elif put_response.status_code == 404:
+            raise ValueError(
+                (
+                    f"A subscription named {self.subscription_name} does not exist"
+                    "in the Google Cloud Platform project "
+                    f"{settings.GOOGLE_CLOUD_PROJECT}, "
+                    "and one cannot be create because Pitt-Google does not "
+                    "publish a public topic with the same name."
+                )
+            )
+
+        else:
+            # if the subscription name is invalid, content has helpful info
+            print(put_response.content)
+            put_response.raise_for_status()
+
+    def unpack_and_ack_messages(
         self, response, lighten_alerts=False, callback=None, **kwargs
     ):
-        """Unpack messages in `response`. Run `callback` if present."""
+        """Unpack and acknowledge messages in `response`. Run `callback` if present."""
+        # unpack and run the callback
         msgs = response.json()["receivedMessages"]
-        alerts = []
+        alerts, ack_ids = [], []
         for msg in msgs:
             alert_dict = b64avro_to_dict(msg["message"]["data"])
 
@@ -91,6 +162,10 @@ class PittGoogleConsumer:
 
             if alert_dict is not None:
                 alerts.append(alert_dict)
+            ack_ids.append(msg["ackId"])
+
+        # acknowledge messages so they leave the subscription
+        self._ack_messages(ack_ids)
 
         return alerts
 
@@ -104,6 +179,24 @@ class PittGoogleConsumer:
             {k: alert_dict["candidate"][k] for k in keep_fields["candidate"]}
         )
         return alert_lite
+
+    def _ack_messages(self, ack_ids):
+        response = self.oauth.post(
+            f"{self.subscription_url}:acknowledge", data={"ackIds": ack_ids},
+        )
+        response.raise_for_status()
+
+    def delete_subscription(self):
+        """Delete the subscription, if it exists."""
+        response = self.oauth.delete(self.subscription_url)
+        if response.status_code == 200:
+            self._log_and_print(f"Deleted subscription: {self.subscription_path}")
+        elif response.status_code == 404:
+            print(
+                f"Nothing to delete, subscription does not exist: {self.subscription_path}"
+            )
+        else:
+            response.raise_for_status()
 
     def _log_and_print(self, msg, severity="INFO"):
         # request = {
