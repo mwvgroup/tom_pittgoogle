0b826fcc0bb0f881dc76b90f0ee2906b9f2b6e95
troyraen <troy.raen@pitt.edu>
2021-10-06 15:54:33 -0400

    load apps on init for RTD

--- tom_pittgoogle/__init__.py
+++ tom_pittgoogle/__init__.py
@@ -2,10 +2,10 @@
 # -*- coding: UTF-8 -*-
 """The `tom_pittgoogle` module contains Pitt-Google's TOM broker implementations."""
 
-# from django.core.wsgi import get_wsgi_application
-# import os
+from django.core.wsgi import get_wsgi_application
+import os
 
 
-# if 'BUILD_IN_RTD' in os.environ:
-#     os.environ['DJANGO_SETTINGS_MODULE'] = "tom_pittgoogle.settings"
-#     application = get_wsgi_application()
+if 'BUILD_IN_RTD' in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = "tom_pittgoogle.settings"
+    application = get_wsgi_application()
