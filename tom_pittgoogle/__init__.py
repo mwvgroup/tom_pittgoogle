dc388de95b1cc81f385861dc3cf504e5265fa01c
troyraen <troy.raen@pitt.edu>
2021-10-06 15:38:18 -0400

    trying to fix RTD build

---
+++ tom_pittgoogle/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""The `tom_pittgoogle` module contains Pitt-Google's TOM broker implementations."""
+
+# from django.core.wsgi import get_wsgi_application
+# import os
+
+
+# if 'BUILD_IN_RTD' in os.environ:
+#     os.environ['DJANGO_SETTINGS_MODULE'] = "tom_pittgoogle.settings"
+#     application = get_wsgi_application()
